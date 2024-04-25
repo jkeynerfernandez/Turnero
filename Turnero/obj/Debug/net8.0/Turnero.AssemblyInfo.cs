c5973aae2a9e957fdac6cd8db4c2dcda3f8993e8
jkeynerfernandez <fernandezdj54@gmail.com>
2024-04-24 21:20:26 -0500

    erge

--- Turnero/obj/Debug/net8.0/Turnero.AssemblyInfo.cs
+++ Turnero/obj/Debug/net8.0/Turnero.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Turnero")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5068b5b623049bcd4636b52d9c34f29bb792c533")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+521028cfe95872224c81df198575679e6ed4a8cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Turnero")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Turnero")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
